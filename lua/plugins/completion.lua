6fc973a836f3377e3b78ebf1d33d3ecccd574e3a
Noah Shuart <shuart.noah.s@gmail.com>
2024-12-25 04:40:01 -0700

    tweak completion config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -13,6 +13,10 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+        enabled = function ()
+            return vim.bo.buftype ~= 'prompt' and vim.b.completion ~= false
+        end,
+
         -- 'default' for mappings similar to built-in completion
         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
@@ -20,23 +24,27 @@ return {
         -- your own keymap.
         keymap = { preset = 'super-tab' },
 
-        highlight = {
+        appearance = {
             -- sets the fallback highlight groups to nvim-cmp's highlight groups
             -- useful for when your theme doesn't support blink.cmp
             -- will be removed in a future release, assuming themes add support
             use_nvim_cmp_as_default = true,
-        },
-        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
 
-        -- experimental auto-brackets support
-        accept = { auto_brackets = { enabled = true } },
+            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- adjusts spacing to ensure icons are aligned
+            nerd_font_variant = 'mono'
+        },
 
         -- experimental signature help support
         signature = { enabled = true },
 
         completion = {
+            accept = {
+                -- experimental auto-brackets support
+                auto_brackets = {
+                    enabled = true
+                }
+            },
             trigger = {
                 show_on_insert_on_trigger_character = false,
 
@@ -48,6 +56,10 @@ return {
                 auto_show = true,
                 auto_show_delay_ms = 500
             }
+        },
+
+        fuzzy = {
+            use_typo_resistance = false
         }
     },
     -- allows extending the enabled_providers array elsewhere in your config
