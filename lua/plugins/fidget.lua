e41ba74746d7799587632917366cbfbf0e77747f
Noah Shuart <shuart.noah.s@gmail.com>
2025-12-10 10:39:41 -0700

    updates/tweaks

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,4 @@
+return {
+    'j-hui/fidget.nvim', version = 'v1.*',
+    opts = {}
+}
--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,9 +1,16 @@
 return {
     'stevearc/overseer.nvim', version = 'v1.*',
     keys = {
-        {'<leader>or', function () vim.cmd.OverseerRun() end, desc = 'Run Task'},
-        {'<leader>oa', function () vim.cmd.OverseerTaskAction() end, desc = 'Task Actions'},
-        {'<leader>kt', function () vim.cmd.OverseerToggle() end, desc = 'Toggle Overseer Panel'}
+        {'<leader>or', function () vim.cmd.OverseerRun() end,
+            desc = 'Run Task'},
+        {'<leader>oa', function () vim.cmd.OverseerTaskAction() end,
+            desc = 'Task Actions'},
+        {'<leader>kt', function () require('overseer').toggle({ enter = false}) end,
+            desc = 'Toggle Overseer Panel'}
     },
-    opts = {}
+    opts = {
+        task_list = {
+            direction = "left"
+        }
+    }
 }
